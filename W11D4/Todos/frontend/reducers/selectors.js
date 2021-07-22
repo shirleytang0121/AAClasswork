1ab74873a6ed6f52332fdbb9b31ed5200a04344e
Shirley Tang <shirleytang1996@gmail.com>
2021-07-22 18:53:47 -0400

    delete todos db

--- W11D4/Todos/frontend/reducers/selectors.js
+++
@@ -1,7 +0,0 @@
-export const allTodos = (state) => Object.values(state.todos);
-
-
-export const stepsByTodoId = (state, todoId) => Object.values(state.steps).filter( (step)=> step.todo_id === todoId)
-
-
-
