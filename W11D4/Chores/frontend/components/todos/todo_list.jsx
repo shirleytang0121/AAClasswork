aa7bcb869c87123638af36fa4fdb4aba43e77209
Ezekiel Yu <cellozeke@gmail.com>
2021-07-22 17:16:10 -0700

    last push for todos

--- W11D4/Chores/frontend/components/todos/todo_list.jsx
+++ W11D4/Chores/frontend/components/todos/todo_list.jsx
@@ -3,10 +3,14 @@ import TodoListItem from './todo_list_item';
 import TodoForm from './todo_form';
 
 export default class TodoList extends React.Component {
+  componentDidMount() {
+    this.props.fetchAllTodos();
+  }
+
   render() {
     return(
       <div className='todo-list'>
-        <TodoForm receiveTodo={this.props.receiveTodo}/>
+        <TodoForm receiveTodo={this.props.receiveTodo} createTodo={this.props.createTodo}/>
         <ul className='todo-list-items'>
           {this.props.todos.map((todo)=>
             <TodoListItem key={todo.id} todo={todo} receiveTodo={this.props.receiveTodo} removeTodo={this.props.removeTodo}/>)}
